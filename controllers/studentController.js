051cfce05d2cca70487d9dcf14cc83c2e3bf172d
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-05-19 16:28:30 +0100

    tu tah tah

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -81,3 +81,14 @@ export const getById = async (req, res) => {
         return res.status(500).json({ msg: "Failed to retrieve students", dev: err.message });
     }
 };
+
+export const getAllStudents = async (req, res) => {
+    
+    try {
+        // sort by latest
+        const students = await Student.find().sort({ _id: -1 });
+        return res.status(200).json(students);
+    } catch (err) {
+        return res.status(500).json({ msg: "Failed to retrieve students", dev: err.message });
+    }
+};
